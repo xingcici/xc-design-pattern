cc2a69d3feb81033ad6ab99725e70917b4eb6636
庞海峰 <haifeng.pang@huijie-inc.com>
2018-05-23 18:55:46 +0800

    init project

---
+++ src/main/java/com/elvin/pattern/pattern/XcDesignPatternApplication.java
@@ -0,0 +1,12 @@
+package com.elvin.pattern.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcDesignPatternApplication.class, args);
+	}
+}
